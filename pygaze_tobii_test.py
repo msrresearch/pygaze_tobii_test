8706ce3b8a3b39677070b06d1b463d8137746447
Björn Guth <bjoern.guth@rwth-aachen.de>
2018-01-18 12:33:23 +0100

    Make test acquire and save data

--- pygaze_tobii_test.py
+++ pygaze_tobii_test.py
@@ -1,6 +1,7 @@
 from pygaze import libscreen, eyetracker
 import time
 import argparse
+import csv
 
 parser = argparse.ArgumentParser(description='A small test for pygaze using tobii eyetracker')
 parser.add_argument('-c', '--no_calibrate', default=False, action='store_true', help='skip calibration')
@@ -9,13 +10,19 @@ args = parser.parse_args()
 display = libscreen.Display()
 screen = libscreen.Screen()
 eyetracker = eyetracker.EyeTracker(display)
-screen.draw_text(text='this is just a test')
-display.fill(screen)
-display.show()
-
-time.sleep(2)
 
 if not args.no_calibrate:
     eyetracker.calibrate()
+
+eyetracker.start_recording()
+
+for i in range (60, 0, -1):
+    screen = libscreen.Screen()
+    screen.draw_text('Recording data for {} seconds'.format(i))
+    display.fill(screen)
+    display.show()
+    time.sleep(1)
+
+eyetracker.stop_recording()
 
 display.close()
