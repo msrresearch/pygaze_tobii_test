a66583e877aea226efa144e37bbf0e6c6f56fb3e
Björn Guth <bjoern.guth@rwth-aachen.de>
2018-01-18 13:02:50 +0100

    Add a small analyzis to the test script

--- pygaze_tobii_test.py
+++ pygaze_tobii_test.py
@@ -2,27 +2,54 @@ from pygaze import libscreen, eyetracker
 import time
 import argparse
 import csv
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='A small test for pygaze using tobii eyetracker')
 parser.add_argument('-c', '--no_calibrate', default=False, action='store_true', help='skip calibration')
+parser.add_argument('-d', '--no_data', default=False, action='store_true', help='skip data acquisition')
 args = parser.parse_args()
 
-display = libscreen.Display()
-screen = libscreen.Screen()
-eyetracker = eyetracker.EyeTracker(display)
-
-if not args.no_calibrate:
-    eyetracker.calibrate()
-
-eyetracker.start_recording()
-
-for i in range (60, 0, -1):
+if not args.no_data:
+    display = libscreen.Display()
     screen = libscreen.Screen()
-    screen.draw_text('Recording data for {} seconds'.format(i))
-    display.fill(screen)
-    display.show()
-    time.sleep(1)
+    eyetracker = eyetracker.EyeTracker(display)
 
-eyetracker.stop_recording()
+    if not args.no_calibrate:
+        eyetracker.calibrate()
 
-display.close()
+    eyetracker.start_recording()
+
+    for i in range (60, 0, -1):
+        screen = libscreen.Screen()
+        screen.draw_text('Recording data for {} seconds'.format(i))
+        display.fill(screen)
+        display.show()
+        time.sleep(1)
+
+    eyetracker.stop_recording()
+
+    display.close()
+
+print('##################\n# analyzing data #\n##################')
+
+tsv_list = []
+with open('default_TOBII_output.tsv', 'rb') as tsv_file:
+    tsv_in = csv.reader(tsv_file, delimiter='\t')
+    for row in tsv_in:
+        tsv_list.append(row)
+
+data_list = []
+time_list = []
+for row in tsv_list[8:]:
+    time = float(row[0])
+    if time_list:
+        data_list.append(time-time_list[-1])
+    time_list.append(time)
+
+plt.plot(time_list[1:], data_list, label='durian')
+plt.xlabel('Experiment time (ms)')
+plt.ylabel('Time difference (ms)')
+plt.grid(True)
+
+plt.savefig('plot_time_diffence')
+plt.show()
